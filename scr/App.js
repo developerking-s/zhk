b46c2f98cee7c78431d1ed43de07fc8d8dc4e752
developer_king <renafernandesjorge11@gmail.com>
2025-03-04 13:21:02 +0000

    Create scr/App.js

---
+++ scr/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bem-vindo ao Meu Site!</h1>
+        <p>Este é um exemplo de site criado com React.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
